1bde8ad6e364235737a112469efd0b61930e4512
Zachary Soon <zwsoon@gmail.com>
2020-01-20 20:11:18 -0500

    json object

--- src/test/java/Demo1_GET_REQUEST.java
+++ src/test/java/Demo1_GET_REQUEST.java
@@ -1,32 +1,42 @@
+import org.json.simple.parser.JSONParser;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import junit.framework.Assert;
 
 public class Demo1_GET_REQUEST {
 	
-	String city = "Atlanta";
+	
 	
 	public void getWeatherDetails(String city) {
 		//specify base uri
 		RestAssured.baseURI="http://restapi.demoqa.com/utilities/weather/city";
-
 		//request object
 		RequestSpecification httpRequest = RestAssured.given();
-		
 		//response object
 		Response response = httpRequest.request(Method.GET,"/"+city);
-		String responseBody = response.getBody().asString();
-		//print the response
-		System.out.println(responseBody);
+		if(response.getStatusCode() == 200) {
+			Assert.assertEquals(200, response.getStatusCode());
+			System.out.println("Status code is: " + response.getStatusCode());
+		}else {
+			System.out.print("Did not return Status 200");
+		}
+		//print the status code		
+		response.getBody().prettyPrint();
+	}
+	
+	public void readJSON() {
+		JSONParser parser = new JSONParser();
 		
 	}
 	
 	//calling method and passing in parameter
 	@Test
 	public void getCityWeather() {
+		String city = "Atlanta";
 		getWeatherDetails(city);
 	}
 }
