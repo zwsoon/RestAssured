c8a4170f2430228867ef635da62e348cd3f001a5
Zachary Soon <zwsoon@gmail.com>
2020-01-20 22:17:25 -0500

    json file to read and json parser

--- src/test/java/Demo1_GET_REQUEST.java
+++ src/test/java/Demo1_GET_REQUEST.java
@@ -1,3 +1,8 @@
+import java.io.FileReader;
+import java.util.Iterator;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.testng.annotations.Test;
 
@@ -30,6 +35,29 @@ public class Demo1_GET_REQUEST {
 	
 	public void readJSON() {
 		JSONParser parser = new JSONParser();
+		 
+        try {
+ 
+            Object obj = parser.parse(new FileReader(
+                    "C:\\Users\\zwsoo\\eclipse-workspace\\Testing\\src\\test\\java\\resource"));
+ 
+            JSONObject jsonObject = (JSONObject) obj;
+ 
+            String name = (String) jsonObject.get("Name");
+            String author = (String) jsonObject.get("Author");
+            JSONArray companyList = (JSONArray) jsonObject.get("Company List");
+ 
+            System.out.println("Name: " + name);
+            System.out.println("Author: " + author);
+            System.out.println("\nCompany List:");
+            Iterator<String> iterator = companyList.iterator();
+            while (iterator.hasNext()) {
+                System.out.println(iterator.next());
+            }
+ 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 		
 	}
 	
